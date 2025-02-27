208b92703ae29bc284f8e7c991e64278fd84d097
stan-skorin <savelevstan@gmail.com>
2025-02-27 10:05:55 -0500

    Initial commit

---
+++ src/main/java/com/sandbox/springsecuritysandbox/SpringSecuritySandboxApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.springsecuritysandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sandbox/springsecuritysandbox/SpringSecuritySandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.springsecuritysandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
